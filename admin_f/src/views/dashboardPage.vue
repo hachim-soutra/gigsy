bda781404d5123166f0f6f14b36a7588d6252f6f
amine aqebli <aqebli36@gmail.com>
2022-05-12 23:58:26 +0100

    show users counter in dashboard

--- admin_f/src/views/dashboardPage.vue
+++ admin_f/src/views/dashboardPage.vue
@@ -5,7 +5,7 @@
         <div class="container">
         <div class="row">
             <base-card :shadow="true" title="Surbrillance">
-                <number-card color="bg-c-yellow" icon="fa-users" title="Nombre d'utilisateur" number="50"></number-card>
+                <number-card color="bg-c-yellow" icon="fa-users" title="Nombre d'utilisateur" :number="usersCounter"></number-card>
                 <number-card color="bg-c-pink" icon="fa-shop" title="Nombre de vendeurs" number="50"></number-card>
                 <number-card color="bg-c-green" icon="fa-cart-flatbed" title="Nombre d'acheteurs" number="500"></number-card>
                 <number-card color="bg-c-blue" icon="fa-chart-line" title="Nombre de commandes" number="450"></number-card>
@@ -19,6 +19,22 @@
 <script>
 import NumberCard from '../components/ui/card/NumberCard.vue'
 export default {
-    components:{NumberCard}
+    components:{NumberCard},
+    data(){
+        return{
+            usersCounter: 0
+        }
+    },
+    created(){
+        this.userCounter();
+    },
+    methods:{
+        userCounter(){
+            this.usersCounter = this.$store.getters['users/users'].length;
+        }
+    },
+    computed:{
+    
+    }
 }
 </script>
