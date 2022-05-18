f7ed19ba19fb12664d651671165222aa922d1afd
hachim-soutra <hachimsoutra10@gmail.com>
2022-05-18 23:10:18 +0200

    add vuex

--- gigsy-f/src/router/index.js
+++ gigsy-f/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import HomeRoutes from "./home";
+// import checkAuth from "../middlewares/checkAuth";
+// const isAuthenticated = true;
 
 Vue.use(Router);
 
@@ -10,8 +12,13 @@ let router = new Router({
   mode: "history",
   routes: routes,
   linkExactActiveClass: "active",
-  scrollBehavior: function(to, from, savedPosition) {
+  scrollBehavior: function (to, from, savedPosition) {
     return savedPosition || { x: 0, y: 0 };
   },
 });
+
+// router.beforeEach((to, from, next) => {
+//   checkAuth(next, isAuthenticated);
+// });
+
 export default router;
