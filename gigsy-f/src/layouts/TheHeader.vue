22196ac8cce139c9a0609d9f7f5bc532a4740eb2
n-azed <96393153+n-azed@users.noreply.github.com>
2022-05-28 13:40:27 +0100

    add-page-service

--- gigsy-f/src/layouts/TheHeader.vue
+++ gigsy-f/src/layouts/TheHeader.vue
@@ -1,65 +1,34 @@
 <template>
   <header id="header" class="fixed-top">
-    <div class="container d-flex">
+    <div class="container-fluid d-flex">
+
       <div class="logo mr-auto">
-        <h1 class="text-light">
-          <router-link :to="{ name: 'home' }"
-            ><span>Ninestars</span></router-link
-          >
-        </h1>
+        <h1 class="text-light"><router-link :to="{name:'home'}"><span>Gigsy</span></router-link></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
-          <li><a href="#header">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
+          <li ><a href="#header">Accueil</a></li>
+          <li><a href="#about">A-propos</a></li>
+          <li><router-link :to="{name:'services'}">Services</router-link></li>
+          <li><a href="#portfolio">Portefeuille</a></li>
+          <li><a href="#team">Equipe</a></li>
+  
+          <li><a href="#contact">Contactez-nous</a></li>
 
-          <li><a href="#contact">Contact Us</a></li>
-          <template v-if="!user.data">
-            <li class="get-started">
-              <router-link :to="{ name: 'login' }">Login</router-link>
-            </li>
-            <li>
-              <router-link :to="{ name: 'register' }">Register</router-link>
-            </li>
-          </template>
-          <template v-else>
-            <li class="dropdown">
-              <button
-                class="btn btn-default profile-img"
-                type="button"
-                data-toggle="dropdown"
-              >
-                {{ user.data.first_name | TwoLetter }}
-              </button>
-              <ul class="dropdown-menu">
-                <li><a href="#">My profil</a></li>
-                <li><a href="#">Log out</a></li>
-              </ul>
-            </li>
-          </template>
+          <li class="get-started"><router-link :to="{name:'login'}">S'identifier</router-link></li>
+          <li><router-link :to="{name:'register'}">S'inscrire</router-link></li>
         </ul>
-      </nav>
-      <!-- .nav-menu -->
+      </nav><!-- .nav-menu -->
+
     </div>
   </header>
 </template>
 
 <script>
-import userMixin from "../common/mixins/user.js";
-export default {
-  mixins: [userMixin],
-  filters: {
-    TwoLetter: function (value) {
-      if (!value) return "";
-      value = value.toString();
-      return value.substring(0, 2);
-    },
-  },
-};
+export default {};
 </script>
 
 <style scoped>
@@ -72,7 +41,8 @@ export default {
   background-color: var(--secondary-color);
   padding: 8px 20px;
   border-radius: 20px;
-  cursor: pointer;
+    cursor: pointer;
+
 }
 .navbar-handler {
   align-items: center;
@@ -80,17 +50,5 @@ export default {
   width: 100%;
   display: flex;
   justify-content: end;
-}
-.profile-img {
-  -webkit-border-radius: 50%;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--secondary-color);
-  color: white;
-  height: 45px;
-  width: 45px;
-  cursor: pointer;
 }
 </style>
--- gigsy-f/src/views/services/services.vue
+++ gigsy-f/src/views/services/services.vue
@@ -3,113 +3,32 @@
     <!-- ======= Services Section ======= -->
     <section id="services" class="services section-bg">
       <div class="container">
-
         <div class="section-title" data-aos="fade-up">
           <p>Check out the great services we offer</p>
         </div>
-
-
-        <VueSlickCarousel v-bind="settings" :style="{ height: '372px' }">
-
-          <router-link :to="{ name: 'seo-services' }">
+        <div class="row">
+     <router-link :to="{ name: 'seo-services'}" class="col-6 col-md-4 col-lg-3" v-for="service in services" :key="service.$key">
             <div class="container">
               <img
                 src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_550,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741668/seo-2x.png"
                 alt="seo" class="img-fluid" style="width:100%;">
-              <div class="top-left">SEO</div>
+              <div class="top-left">{{service}}</div>
             </div>
           </router-link>
-
-          <div class="container">
-            <img
-              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_550,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741664/illustration-2x.png"
-              alt="illustration" class="img-fluid" style="width:100%;">
-            <div class="top-left">ILLUSTATION</div>
-          </div>
-
-
-
-          <div class="container">
-            <img
-              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_550,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741674/translation-2x.png"
-              alt="translation" class="img-fluid" style="width:100%;">
-            <div class="top-left">TRANSLATION</div>
-          </div>
-
-
-
-          <div class="container">
-            <img
-              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_550,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741664/data-entry-2x.png"
-              alt="data" class="img-fluid" style="width:100%;">
-            <div class="top-left">DATA ENTRY</div>
-          </div>
-
-
-
-
-          <div class="container">
-            <img
-              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_550,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/book-covers-2x.png"
-              alt="book" class="img-fluid" style="width:100%;">
-            <div class="top-left">BOOK COVERS</div>
-          </div>
-
-
-
-
-          <div class="container">
-            <img
-              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_550,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png"
-              alt="wordpress" class="img-fluid" style="width:100%;">
-            <div class="top-left">WORDPRESS</div>
-          </div>
-
-
-
-          <div class="container">
-            <img
-              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_550,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png"
-              alt="voiceover" class="img-fluid" style="width:100%;">
-            <div class="top-left">VOICE OVER</div>
-          </div>
-
-
-
-          <div class="container">
-            <img
-              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_550,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png"
-              alt="animated" class="img-fluid" style="width:100%;">
-            <div class="top-left">ANIMATED EXPLAINER</div>
-          </div>
-
-
-          <div class="container">
-            <img
-              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_550,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png"
-              alt="social" class="img-fluid" style="width:100%;">
-            <div class="top-left">SOCIAL MEDIA</div>
-          </div>
-
-        </VueSlickCarousel>
-
-
+        </div>
+     
       </div>
     </section>
   </div>
 </template>
 
 <script>
-import VueSlickCarousel from 'vue-slick-carousel'
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-// optional style for arrows & dots
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
 export default {
   name: 'MyComponent',
-  components: { VueSlickCarousel },
   data() {
     return {
+        services: ["seo", "illus", "Translate"],
       settings: {
         arrows: true,
         dots: true,
