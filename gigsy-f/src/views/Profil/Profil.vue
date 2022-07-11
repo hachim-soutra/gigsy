7b162bf39509bb8daef843003c1d214d7e191e76
n-azed <96393153+n-azed@users.noreply.github.com>
2022-07-11 01:35:33 +0100

    profil-page-changes

--- gigsy-f/src/views/Profil/Profil.vue
+++ gigsy-f/src/views/Profil/Profil.vue
@@ -1,111 +1,175 @@
 <template>
-  <div class="container-fluid log-reg-container">
-    <section class="login-regester mt-lg-5">
-      <div class="row d-flex d-flex justify-content-around p-3">
-        <div
-          class="col-lg-5 col-md-10 mt-5 mt-lg-0 align-items-center border rounded p-5"
-          data-aos="fade-up"
-          data-aos-delay="200"
-        >
-          <img
-            class=" animated profil-pic"
-            src="assets\img\1549311939801.jpg"
-          />
-          <form
-            @submit.prevent="register"
-            @keydown="form.onKeydown($event)"
-            class="php-email-form w-100"
+  <div class="container-fluid ctf1 my-3">
+    <div class="row justify-content-center">
+      <div
+        class="col-lg-7 col-md-12 col-sm-12 mx-4 rounded p-3"
+        data-aos="fade-up"
+        data-aos-delay="200"
+      >
+        <div class="row border">
+          <div
+            class="col-lg-5 col-md-12 p-2 rounded-top text-center"
+            style="height: 300px"
           >
-            <div class="form-group">
-              <label for="name">Votre nom</label>
-              <input
-                type="text"
-                class="form-control"
-                v-model="form.first_name"
-                :class="{ 'is-invalid': form.errors.has('first_name') }"
-              />
-              <div
-                class="invalid-feedback text-left"
-                v-if="form.errors.has('first_name')"
-              >
-                {{ form.errors.get("first_name") }}
-              </div>
+            <img
+              class="rounded-circle mx-auto"
+              src="assets\img\1549311939801.jpg"
+              max-width="auto"
+              height="100%"
+              style="margin-top: -14%"
+            />
+            <div
+              class="rounded-circle position-relative mx-auto pt-3"
+              style="
+                background-color: rgb(0, 179, 255);
+                width: 100px;
+                height: 100px;
+                margin-top: -10%;
+                color: white;
+              "
+            >
+              date d'inscription {{ date }}
             </div>
-            <div class="form-group">
-              <label for="name">Votre Email</label>
-              <input
-                disabled
-                type="email"
-                class="form-control"
-                name="email"
-                id="email"
-                v-model="form.email"
-                :class="{ 'is-invalid': form.errors.has('email') }"
-              />
-              <div
-                class="invalid-feedback text-left"
-                v-if="form.errors.has('email')"
-              >
-                {{ form.errors.get("email") }}
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label for="name">Votre nouveau mot de passe</label>
-              <input
-                type="password"
-                class="form-control"
-                name="password"
-                id="password"
-                v-model="form.password"
-                :class="{ 'is-invalid': form.errors.has('password') }"
-              />
-              <div
-                class="invalid-feedback text-left"
-                v-if="form.errors.has('password')"
-              >
-                {{ form.errors.get("password") }}
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="name">Répéter le nouveau mot de passe</label>
-              <input
-                type="password"
-                class="form-control"
-                name="re-password"
-                id="re-password"
-                v-model="form.password_confirmation"
-                :class="{
-                  'is-invalid': form.errors.has('password_confirmation'),
-                }"
-              />
-              <div
-                class="invalid-feedback text-left"
-                v-if="form.errors.has('password_confirmation')"
-              >
-                {{ form.errors.get("password_confirmation") }}
-              </div>
-            </div>
-            <div class="text-center">
-              <button type="submit" :disabled="form.busy" class="mt-3">
-                Modifier
-              </button>
-            </div>
-          </form>
+          </div>
+          <div class="col-lg-7 col-md-12 p-2 pt-5">
+            <h1 class="text-lg-left text-md-center">{{ fullname }}</h1>
+            <h2 class="text-lg-left text-md-center">
+              {{ telephone }}
+            </h2>
+            <h2 class="text-lg-left text-md-center">{{ emailp }}</h2>
+          </div>
         </div>
-        <div
-          class="col-lg-5 col-md-10 mt-5 mt-lg-0 align-items-center border rounded p-5"
-          data-aos="fade-up"
-          data-aos-delay="200"
-        >
-          <h2 class="mb-5">Mes Gigs</h2>
-          <p v-if="mygigs.length == 0">Vous n'avez aucun Gig</p>
-          <ul v-else>
-            <li v-for="mygig in mygigs" :key="mygig.$key">{{ mygig }}</li>
-          </ul>
+        <div class="row p-4">
+          <h2 v-if="mygigs.length === 0">Vous n'a Aucun Gig</h2>
+          <div class="row p-4" v-else>
+            <h2 class="p-4">Vous avez a {{ mygigs.length }} Gigs</h2>
+            <table class="table">
+              <caption>
+                La liste des Gigs de
+                {{
+                  fullname
+                }}
+              </caption>
+              <thead>
+                <tr>
+                  <th scope="col"><h4>#</h4></th>
+                  <th scope="col"><h4>Titre</h4></th>
+                  <th scope="col"><h4>Description</h4></th>
+                  <th scope="col"><h4>Prix</h4></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(mygig, index) in mygigs" :key="mygig.index">
+                  <th scope="row">
+                    <h5>{{ index + 1 }}</h5>
+                  </th>
+                  <td>
+                    <h5>{{ mygig.title }}</h5>
+                  </td>
+                  <td>
+                    <h5>{{ mygig.description }}</h5>
+                  </td>
+                  <td>
+                    <h5>{{ mygig.price }}</h5>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </section>
+      <div
+        class="col-lg-4 p-3 mx-4 rounded align-items-center border"
+        style="background-color: white; max-height: 32rem"
+        data-aos="fade-up"
+        data-aos-delay="200"
+      >
+        <h4 class="p-3" style="background-color: #e9ecef; text-align: center">
+          Modifiez vos coordonnés :
+        </h4>
+        <form
+          @submit.prevent="register"
+          @keydown="form.onKeydown($event)"
+          class="php-email-form w-100"
+        >
+          <div class="form-group">
+            <label for="name">Votre nom</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="form.first_name"
+              :class="{ 'is-invalid': form.errors.has('first_name') }"
+            />
+            <div
+              class="invalid-feedback text-left"
+              v-if="form.errors.has('first_name')"
+            >
+              {{ form.errors.get("first_name") }}
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="name">Votre Email</label>
+            <input
+              disabled
+              type="email"
+              class="form-control"
+              name="email"
+              id="email"
+              v-model="form.email"
+              :class="{ 'is-invalid': form.errors.has('email') }"
+            />
+            <div
+              class="invalid-feedback text-left"
+              v-if="form.errors.has('email')"
+            >
+              {{ form.errors.get("email") }}
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="name">Votre nouveau mot de passe</label>
+            <input
+              type="password"
+              class="form-control"
+              name="password"
+              id="password"
+              v-model="form.password"
+              :class="{ 'is-invalid': form.errors.has('password') }"
+            />
+            <div
+              class="invalid-feedback text-left"
+              v-if="form.errors.has('password')"
+            >
+              {{ form.errors.get("password") }}
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="name">Répéter le nouveau mot de passe</label>
+            <input
+              type="password"
+              class="form-control"
+              name="re-password"
+              id="re-password"
+              v-model="form.password_confirmation"
+              :class="{
+                'is-invalid': form.errors.has('password_confirmation'),
+              }"
+            />
+            <div
+              class="invalid-feedback text-left"
+              v-if="form.errors.has('password_confirmation')"
+            >
+              {{ form.errors.get("password_confirmation") }}
+            </div>
+          </div>
+          <div class="text-center">
+            <button type="submit" :disabled="form.busy" class="mt-3">
+              Modifier
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -119,7 +183,32 @@ export default {
       password: "",
       password_confirmation: "",
     }),
-    mygigs: ["gig1", "gig2", "gig3", "gig4"],
+    mygigs: [
+      {
+        title: "AAAA",
+        description: "aaaa aaaa aaaaa aaaaaaa aaaaaaaaa aaaaaa",
+        price: 20,
+      },
+      {
+        title: "BBBB",
+        description: "bbbb bbbb bbbbb bbbbbb bbbbbbbb bbbbbbb",
+        price: 30,
+      },
+      {
+        title: "CCCC",
+        description: "cccc cccc ccccc ccccccc ccccccccc cccccc",
+        price: 60,
+      },
+      {
+        title: "DDDD",
+        description: "dddd dddd ddddd ddddddd ddddddddd dddddd",
+        price: 80,
+      },
+    ],
+    date: "12/12/22",
+    fullname: "Hachim Soutra",
+    telephone: "06 00 00 00 00",
+    emailp: "hachim.soutra@gigsy.com",
   }),
 
   methods: {
@@ -138,6 +227,9 @@ export default {
 <style scoped>
 label {
   float: left;
+}
+.ctf1 {
+  margin-top: 9rem !important;
 }
 button {
   background: #eb5d1e;
